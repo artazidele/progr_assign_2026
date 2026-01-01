4cd17f73a999646a82e0f73bc4e3ffac34a8188e
artazidele <77989249+artazidele@users.noreply.github.com>
2026-01-01 19:05:00 +0200

    fix: fix jar fail and resource fail names and destination directories

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        var graph = new Graph("src/main/resources/input3.txt");
+        var graph = new Graph("input3.txt");
         var algorithm = new MaximumSpanningTree(graph);
         algorithm.findMaximumSpanningTree();
-        graph.writeToFile("src/main/resources/output.txt");
+        graph.writeToFile("output.txt");
     }
 }
