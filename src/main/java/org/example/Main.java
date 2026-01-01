6caa4b87407f0186e721fb724fc2c4c5dc2a1c1c
artazidele <77989249+artazidele@users.noreply.github.com>
2026-01-01 18:42:39 +0200

    add jar fail with mvn package

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        var graph = new Graph("src/main/resources/input.txt");
+        var graph = new Graph("src/main/resources/input3.txt");
         var algorithm = new MaximumSpanningTree(graph);
         algorithm.findMaximumSpanningTree();
         graph.writeToFile("src/main/resources/output.txt");
