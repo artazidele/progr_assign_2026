a056912253a267f90797bb6421aecc9e02de33a7
artazidele <77989249+artazidele@users.noreply.github.com>
2026-01-01 19:30:07 +0200

    fix: fix input fail name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        var graph = new Graph("input3.txt");
+        var graph = new Graph("input.txt");
         var algorithm = new MaximumSpanningTree(graph);
         algorithm.findMaximumSpanningTree();
         graph.writeToFile("output.txt");
